773531501fc15fcedcae0391981ca74b4880b0dd
Shakilrev <134630735+Shakilrev@users.noreply.github.com>
2023-10-07 21:58:58 +0600

    Add files via upload

---
+++ php Lab Task -3/part-5/B/index.php
@@ -0,0 +1,32 @@
+<?php
+    if(isset($_REQUEST['submit'])){
+        $degrees = $_REQUEST['degrees'];
+        
+        echo "Selected Degree:<br>";
+        foreach($degrees as $degree){
+            echo $degree."<br>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>5B</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <fieldset>
+            <legend>Degree</legend>
+            <input type="checkbox" name="degrees[]" value="ssc" id=""><label>SSC</label>
+            <input type="checkbox" name="degrees[]" value="hsc" id=""><label>HSC</label>
+            <input type="checkbox" name="degrees[]" value="bsc" id=""><label>BSc</label>
+            <input type="checkbox" name="degrees[]" value="msc" id=""><label>MSc</label>
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+</html>
